a0ed7bc3b32d3e880b928ab8abefa321c7ba0701
wpearson-web <pearson.willy@gmail.com>
2020-10-13 19:09:13 -0500

    module updates

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,16 @@
 var playerName = window.prompt("What is your robot's name?");
-var name = "your name";
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message.")
-// this will do math and log 20
-console.log(10 + 10);
-// what is this?
-console.log("our robot's name is " + playerName);
-console.log(name)
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
-// this creates a function named "fight"
-function fight() {
-    window.alert("The fight has begun!");
-}
-// fight();
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyattack = 12;
+
+var fight = function () {
+    window.alert("Welcome to Robot Gladiators!");
+};
+
+fight();
